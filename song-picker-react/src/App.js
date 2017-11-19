1d2a93d4a0703615893c44e997e112bec9e3c7e6
Daniel Park <dpeace926@gmail.com>
2017-11-18 23:33:47 -0500

    build website layout

--- song-picker-react/src/App.js
+++ song-picker-react/src/App.js
@@ -6,13 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>Title</div>
+        <div>Artist</div>
+        <div>Album</div>
+        <div>Label</div>
+        <div>Year</div>
+        <div>Key</div>
+        <div>
+          <div>BPM</div>
+          <div>Slow</div>
+          <div>Medium</div>
+          <div>Fast</div>
+        </div>
+
+        <div>Transpose</div>
+        <div>Lyrics Only</div>
+
+        <div>Add to list</div>
+
+        <div>Listen</div>
       </div>
     );
   }
