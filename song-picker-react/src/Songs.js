49cd6195e9360ac11620fe9949d20cc6c4f238e8
Daniel Park <dpeace926@gmail.com>
2017-11-18 23:34:25 -0500

    create Songs file for JSON example

---
+++ song-picker-react/src/Songs.js
@@ -0,0 +1,16 @@
+const songs = [
+  {
+    "Title": "What a Beautiful Name",
+    "Author(s)": [
+      "Ben Fielding",
+      "Brooke Ligertwood"
+    ],
+    "Producer(s)": []
+    "Album": "Let There Be Light",
+    "Label": "Hillsong Worship",
+    "Year": "2016",
+    "Time Sig": "4/4"
+    "Key": "D",
+    "BPM": "64"
+  }
+]
